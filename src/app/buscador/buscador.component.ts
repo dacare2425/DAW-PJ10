1475e7baebda0251cbc193ab928f0bbfbea11942
dacare2425 <davidcalpar@gmail.com>
2025-02-04 21:29:18 +0100

    Buscador imprime texto correctamente. Faltan filtros

--- src/app/buscador/buscador.component.ts
+++ src/app/buscador/buscador.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
-import data from '../../../public/data/pj10.json';
+import * as data from '../../../public/pj10.json';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-buscador',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports: [FormsModule,CommonModule] ,
   templateUrl: './buscador.component.html',
   styleUrl: './buscador.component.css'
 })
 export class BuscadorComponent {
-  paises: any[] = data.paises;
-  filtrarPorPais(){
-    return this.paises;
+  textInput: string = '';
+  jsonData = (data as any).default;
+  paisesFiltrados: string = '';
+
+  constructor() {
+    console.log(this.jsonData);
   }
-}
+
+  buscar() {
+    console.log("Buscando: ", this.textInput);
+    const resultado = this.filtraPais(this.textInput);
+    this.paisesFiltrados = JSON.stringify(resultado, null, 2);
+    console.log(resultado);
+  }
+
+  filtraPais(paisBuscado: string) {
+    return this.jsonData.paises.filter((item: { nombre: string }) =>
+      typeof item === 'object' &&
+      item.nombre.toLowerCase() === paisBuscado.toLowerCase()
+    );
+  }
+}    
